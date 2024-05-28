540e77ba02d90c0b4989b0b355a9c765efea20b9
frederikhendrix <rikhendrix@live.nl>
2024-05-28 22:23:14 +0200

    testing running npm run build and after uploading it to github

--- interactiveflix/src/components/Dashboard.js
+++ interactiveflix/src/components/Dashboard.js
@@ -26,7 +26,7 @@ const Dashboard = () => {
 
           if (!response.ok) {
             throw new Error(
-              `HTTP error! status: ${response.status}, body: ${responseText}`
+              `HTTP error status: ${response.status}, body: ${responseText}`
             );
           }
 
